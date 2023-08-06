c64f677c2aa83db1f2d32561840ba95faaabcb4b
Giancarlo Mennillo <giancarlo.mennillo@safectory.com>
2023-08-06 23:54:20 +0200

    make fresh new UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,23 @@
-// main.dart
-
-import 'dart:io';
-
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:workmanager/workmanager.dart';
-
-import 'JournalingScreen.dart';
-
-
-
-void callbackDispatcher() {
-  Workmanager().executeTask((task, inputData) {
-    if (task == 'mindfulness_timer_task') {
-      int durationInSeconds = 10;
-      while (durationInSeconds > 0) {
-        durationInSeconds--;
-        sleep(const Duration(seconds: 1)); // Simulate a one-second delay
-      }
-
-      // Play the sound when the timer completes
-      AudioCache audioCache = AudioCache();
-      audioCache.load('audio/timer_complete.mp3');
-    }
-    return Future.value(true);
-  });
-}
+import 'package:flutter/services.dart';
+import './screens/home.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  Workmanager().initialize(callbackDispatcher);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: JournalingScreen(),
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ToDo App',
+      home: Home(),
     );
   }
 }
