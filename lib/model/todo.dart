b172abcf8f3c971c786dd904a77390c2b257db5b
Giancarlo Mennillo <giancarlo.mennillo@safectory.com>
2023-08-11 00:48:46 +0200

    update todo class in order to operate with recordDate

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -2,11 +2,13 @@ class ToDo {
   int id;
   String todoText;
   bool isDone;
+  String recordDate; // Add this field
 
   ToDo({
     required this.id,
     required this.todoText,
     this.isDone = false,
+    required this.recordDate, // Initialize this field
   });
 
   Map<String, dynamic> toMap() {
@@ -14,6 +16,7 @@ class ToDo {
       'id': id,
       'todoText': todoText,
       'isDone': isDone ? 1 : 0,
+      'recordDate': recordDate,
     };
   }
 
@@ -22,7 +25,7 @@ class ToDo {
       id: map['id'],
       todoText: map['todoText'],
       isDone: map['isDone'] == 1, // Convert the int value to bool
+      recordDate: map['recordDate'],
     );
   }
-
 }
