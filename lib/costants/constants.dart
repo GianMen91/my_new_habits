84bcd19490dfd26e644624566c575e0b003c60e5
GianMen91 <giancarlo.mennillo@gmail.com>
2024-01-14 02:09:41 +0100

    make selectedColor a bit darker

--- lib/costants/constants.dart
+++ lib/costants/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const defaultPadding = 20.0;
 
 const backgroundColor = Color(0xFFFFFBF5);
-const selectedColor =  Color(0xFFFFCCB4);
+const selectedColor =  Color(0xFFFCB79A);
 const touchedBarColor = Color(0xFFA2E0D5);
 const touchedBarColorDarker = Color(0xFFFFFBF5);
 
