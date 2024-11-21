c006a9c68e33b70130206f8c077a570d62ee34df
GianMen91 <giancarlo.mennillo@gmail.com>
2024-11-21 13:32:46 +0100

    improve code

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -3,8 +3,6 @@ import '../costants/constants.dart';
 import '../helpers/database_helper.dart';
 import '../models/todo.dart';
 
-DateTime scheduleTime = DateTime.now();
-
 class SettingsView extends StatefulWidget {
   final Future<void> Function() onFavouriteChange;
 
@@ -15,120 +13,27 @@ class SettingsView extends StatefulWidget {
 }
 
 class _SettingsViewState extends State<SettingsView> {
-  List<ToDo> todosList = [];
+  List<ToDo> _todos = [];
 
   @override
   void initState() {
-    _loadTodosFromDatabase();
     super.initState();
+    _loadTodosFromDatabase();
   }
 
   Future<void> _loadTodosFromDatabase() async {
     final todos = await DatabaseHelper.instance.getTodos();
     setState(() {
-      todosList = todos;
+      _todos = todos;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: backgroundColor,
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 15,
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.arrow_back),
-                        onPressed: () async {
-                          for (ToDo todo in todosList) {
-                            await DatabaseHelper.instance
-                                .updateFavouriteStatus(todo);
-                          }
-
-                          Navigator.pop(context);
-                          widget.onFavouriteChange();
-                        },
-                      ),
-                      const Text(
-                        'Settings',
-                        style: TextStyle(
-                          //fontFamily: 'Niconne',
-                          //fontSize: 32,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(
-                        right: 15, left: 15, top: 10, bottom: 20),
-                    child: Text(
-                      'Choose the habits you want to follow:',
-                      style: TextStyle(
-                        fontSize: 17,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: ListView(
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.only(
-                            bottom: 20,
-                          ),
-                          child: const Column(
-                            
-                          ),
-                        ),
-                        for (ToDo todo in todosList)
-                          Container(
-                            margin: const EdgeInsets.only(bottom: 5),
-                            child: ListTile(
-                              onTap: () {
-                                _onChecked(todo);
-                              },
-                              leading: Icon(
-                                todo.isFavourite
-                                    ? Icons.check_box
-                                    : Icons.check_box_outline_blank,
-                                color: selectedColor,
-                              ),
-                              title: Text(
-                                todo.todoText,
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ),
-                          ),
-                      ],
-                    ),
-                  ),
-                  FloatingActionButton.small(
-                    backgroundColor: selectedColor,
-                    onPressed: () {
-                      _showAddHabitDialog(context);
-                    },
-                    child: const Icon(Icons.add),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+  void _onChecked(ToDo todo) async {
+    setState(() {
+      todo.isFavourite = !todo.isFavourite;
+    });
+    await DatabaseHelper.instance.updateFavouriteStatus(todo);
+    widget.onFavouriteChange(); // Notify parent widget about the change
   }
 
   void _showAddHabitDialog(BuildContext context) {
@@ -138,23 +43,17 @@ class _SettingsViewState extends State<SettingsView> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Add New Habit',
-              style: TextStyle(
-                fontFamily: 'Niconne',
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              )),
+          title: const Text(
+            'Add New Habit',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
           content: TextField(
             controller: habitController,
             style: const TextStyle(color: Colors.black),
-            // Change text color as needed
             decoration: const InputDecoration(
               labelText: 'Enter the new habit',
               labelStyle: TextStyle(color: Colors.black),
-              // Change label text color
-              focusedBorder: UnderlineInputBorder(
-                
-              ),
+              focusedBorder: UnderlineInputBorder(),
               enabledBorder: UnderlineInputBorder(
                 borderSide: BorderSide(color: Colors.grey),
               ),
@@ -165,51 +64,121 @@ class _SettingsViewState extends State<SettingsView> {
               onPressed: () {
                 Navigator.pop(context);
               },
-              child: const Text('Cancel',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: boldTextColor,
-                  )),
+              child: const Text(
+                'Cancel',
+                style: TextStyle(fontWeight: FontWeight.bold, color: boldTextColor),
+              ),
             ),
             TextButton(
-                onPressed: () async {
-                  String newHabitText = habitController.text.trim();
+              onPressed: () async {
+                String newHabitText = habitController.text.trim();
 
-                  if (newHabitText.isNotEmpty) {
-                    // Add new habit to the database
-                    await DatabaseHelper.instance.addNewHabit(newHabitText);
-
-                    // Reload the list of habits
-                    _reloadHabitsList();
-
-                    Navigator.pop(context); // Close the dialog
-                  }
-                },
-                child: const Text('Add',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: boldTextColor,
-                    ))),
+                if (newHabitText.isNotEmpty) {
+                  // Add new habit to the database
+                  await DatabaseHelper.instance.addNewHabit(newHabitText);
+                  _reloadHabitsList();
+                  Navigator.pop(context); // Close the dialog
+                }
+              },
+              child: const Text(
+                'Add',
+                style: TextStyle(fontWeight: FontWeight.bold, color: boldTextColor),
+              ),
+            ),
           ],
         );
       },
     );
   }
 
-  void _reloadHabitsList() async {
-    // Reload the list of habits from the database
+  Future<void> _reloadHabitsList() async {
     List<ToDo> updatedHabits = await DatabaseHelper.instance.getTodos();
-
-    // Update the state to trigger a rebuild of the widget
     setState(() {
-      todosList = updatedHabits;
+      _todos = updatedHabits;
     });
   }
 
-  void _onChecked(ToDo todo) async {
-    setState(() {
-      todo.isFavourite = !todo.isFavourite;
-    });
-    widget.onFavouriteChange(); // Notify the parent widget about the change
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Column(
+            children: [
+              _buildHeader(context),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+                child: Text(
+                  'Choose the habits you want to follow:',
+                  style: TextStyle(fontSize: 17),
+                ),
+              ),
+              _buildHabitList(),
+              _buildAddHabitButton(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () async {
+            for (var todo in _todos) {
+              await DatabaseHelper.instance.updateFavouriteStatus(todo);
+            }
+            Navigator.pop(context);
+            widget.onFavouriteChange();
+          },
+        ),
+        const Text(
+          'Settings',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildHabitList() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: _todos.length,
+        itemBuilder: (context, index) {
+          final todo = _todos[index];
+          return _buildHabitTile(todo);
+        },
+      ),
+    );
+  }
+
+  Widget _buildHabitTile(ToDo todo) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 5),
+      child: ListTile(
+        onTap: () => _onChecked(todo),
+        leading: Icon(
+          todo.isFavourite ? Icons.check_box : Icons.check_box_outline_blank,
+          color: selectedColor,
+        ),
+        title: Text(
+          todo.todoText,
+          style: const TextStyle(fontSize: 16, color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAddHabitButton(BuildContext context) {
+    return FloatingActionButton.small(
+      backgroundColor: selectedColor,
+      onPressed: () => _showAddHabitDialog(context),
+      child: const Icon(Icons.add),
+    );
   }
 }
